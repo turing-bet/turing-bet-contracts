c0d4340b21fea974fb033450ba84fe2d563da11a
C.T. Nava <ctnava97@gmail.com>
2024-09-05 09:35:12 -0700

    change participant minimum & require original participant to start game

--- contracts/DeceptionDerby.sol
+++ contracts/DeceptionDerby.sol
@@ -12,16 +12,16 @@ interface DeceptionDerbyEvents {
     event Withdrawal(address indexed user, uint amount);
 }
 
-contract DeceptionDerby is DeceptionDerbyEvents, Centralized, PlatformFeeManager {
-    function setPlatformFee(uint feePercent) 
-    public override 
-    onlyOwner {
+contract DeceptionDerby is
+    DeceptionDerbyEvents,
+    Centralized,
+    PlatformFeeManager
+{
+    function setPlatformFee(uint feePercent) public override onlyOwner {
         super.setPlatformFee(feePercent);
     }
 
-    constructor(uint _fee) 
-    Centralized(msg.sender) 
-    PlatformFeeManager(_fee) {}
+    constructor(uint _fee) Centralized(msg.sender) PlatformFeeManager(_fee) {}
 
     uint public lobbyIds;
 
@@ -34,85 +34,96 @@ contract DeceptionDerby is DeceptionDerbyEvents, Centralized, PlatformFeeManager
     mapping(uint => address[]) public participantsOf;
     //
     mapping(uint => bool) public locked;
-    // 
+    //
     mapping(uint => bool) public ended;
 
-    function createPublicLobby(uint ante, uint roomLimit)
-    external {
+    function createPublicLobby(uint ante, uint roomLimit) external {
         lobbyIds++;
         creatorOf[lobbyIds] = msg.sender;
         anteOf[lobbyIds] = ante;
         roomLimitFor[lobbyIds] = roomLimit;
+        participantsOf[lobbyId].push(msg.sender);
         emit LobbyCreated(lobbyIds, true);
     }
 
-    function createPrivateLobby(uint ante, address[] memory whitelist)
-    external {
+    function createPrivateLobby(
+        uint ante,
+        address[] memory whitelist
+    ) external {
         lobbyIds++;
         creatorOf[lobbyIds] = msg.sender;
         anteOf[lobbyIds] = ante;
         whitelistFor[lobbyIds] = whitelist;
+        participantsOf[lobbyId].push(msg.sender);
         emit LobbyCreated(lobbyIds, false);
     }
 
-    function isParticipant(uint lobbyId, address user) 
-    internal view returns (bool) {
+    function isParticipant(
+        uint lobbyId,
+        address user
+    ) internal view returns (bool) {
         address[] memory participants = participantsOf[lobbyId];
-        for(uint idx = 0; idx < participants.length; idx++)
+        for (uint idx = 0; idx < participants.length; idx++)
             if (participants[idx] == user) return true;
         return false;
     }
 
-    function authorizedToJoin(uint lobbyId, address user) 
-    internal view returns(bool authorized) {
+    function authorizedToJoin(
+        uint lobbyId,
+        address user
+    ) internal view returns (bool authorized) {
         // bool authorized = false;
         address[] memory participants = participantsOf[lobbyId];
         uint limit = roomLimitFor[lobbyId];
-        if (limit > 0) {  // isPublicLobby
+        if (limit > 0) {
+            // isPublicLobby
             if (participants.length < limit) authorized = true;
-        } else { // isPrivateLobby
+        } else {
+            // isPrivateLobby
             address[] memory whitelist = whitelistFor[lobbyId];
-            for(uint idx = 0; idx < whitelist.length; idx++) 
+            for (uint idx = 0; idx < whitelist.length; idx++)
                 if (whitelist[idx] == user) authorized = true;
         }
         // return authorized;
     }
 
-    function joinGame(uint lobbyId) 
-    external payable {
+    function joinGame(uint lobbyId) external payable {
         require(!locked[lobbyId], "in progress");
-        require(!isParticipant(lobbyId, msg.sender), "already joined"); 
+        require(!isParticipant(lobbyId, msg.sender), "already joined");
         require(authorizedToJoin(lobbyId, msg.sender), "!authorized");
         require(anteOf[lobbyId] == msg.value, "bad msg.value");
         participantsOf[lobbyId].push(msg.sender);
         emit LobbyJoined(lobbyId, msg.sender);
     }
 
-    function startGame(uint lobbyId) 
-    external {
-        require(participantsOf[lobbyId].length > 2, "<3 players");
+    function startGame(uint lobbyId) external {
+        address[] memory participants = participantsOf[lobbyId];
+        require(participants[0] == msg.sender, "!initialParticipant");
+        require(participants.length > 1, "<2 players");
         require(!locked[lobbyId], "in progress");
         locked[lobbyId] = true;
         emit GameStarted(lobbyId);
     }
 
-    mapping(address => uint) public deposits; 
-    
-    function endGame(uint lobbyId, uint[] memory weiAmounts, bytes memory signature)
-    external {
+    mapping(address => uint) public deposits;
+
+    function endGame(
+        uint lobbyId,
+        uint[] memory weiAmounts,
+        bytes memory signature
+    ) external {
         bytes32 hash = keccak256(abi.encodePacked(lobbyId, weiAmounts));
         require(isValidSignature(hash, signature), "!authorized");
         require(locked[lobbyId], "!started");
         require(!ended[lobbyId], "already ended");
         address[] memory participants = participantsOf[lobbyId];
-        for(uint idx = 0; idx < participants.length; idx++)
+        for (uint idx = 0; idx < participants.length; idx++)
             deposits[participants[idx]] += weiAmounts[idx];
         ended[lobbyId] = true;
         emit GameEnded(lobbyId);
     }
 
-    function withdraw() 
-    external {
+    function withdraw() external {
         uint amount = deposits[msg.sender];
         deposits[msg.sender] = 0;
         bool sent = payable(msg.sender).send(amount);

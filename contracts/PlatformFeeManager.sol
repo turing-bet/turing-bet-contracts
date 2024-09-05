5d3a71ed73e1238ddc0535ab3a56d806fb39378d
C.T. Nava <ctnava97@gmail.com>
2024-09-05 09:31:50 -0700

    whitespace

--- contracts/PlatformFeeManager.sol
+++ contracts/PlatformFeeManager.sol
@@ -8,26 +8,22 @@ interface PlatformFeeManagerEvents {
 contract PlatformFeeManager is PlatformFeeManagerEvents {
     uint private _platformFee;
 
-    function platformFee() 
-    public view 
-    returns(uint) {
+    function platformFee() public view returns (uint) {
         return _platformFee;
     }
 
-    function _setPlatformFee(uint feePercent) 
-    private {
+    function _setPlatformFee(uint feePercent) private {
         _platformFee = feePercent;
         emit PlatformFeeUpdated(feePercent);
     }
 
-    function setPlatformFee(uint feePercent) 
-    public virtual {
+    function setPlatformFee(uint feePercent) public virtual {
         _setPlatformFee(feePercent);
     }
 
     // Suggested Value
     // platformFeePercent = 5
-    constructor(uint platformFeePercent) { 
+    constructor(uint platformFeePercent) {
         _setPlatformFee(platformFeePercent);
     }
 }
